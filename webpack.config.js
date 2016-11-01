c26531cf398f03406f9053777ff02ddfd6c2dcbe
Jhonny Michel <jhonnymichel@outlook.com>
2016-11-01 13:25:03 -0300

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: {
+    js: "./app/js/entry.js"
+  },
+  output: {
+    path: __dirname + '/app/dist/js',
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: "node_modules",
+        loader: "babel",
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      }
+    ]
+  }
+
+}
